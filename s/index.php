ffe07a43529463900915233f7eeb821a5246f915
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-01 21:50:03 +0500

    Wrong code: breaking Single Responsibility principle

--- s/index.php
+++ s/index.php
@@ -1,1 +1,40 @@
 <?php
+
+class User
+{
+    public string $name;
+    public string $email;
+
+    public function __construct(array $data)
+    {
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+    }
+
+    public function formatJson()
+    {
+        return json_encode(['name' => $this->name, 'email' => $this->email]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function validate($data)
+    {
+        if (!isset($data['name'])) {
+            throw new Exception("Bad Request, User requires a name");
+        }
+        if (!isset($data['name'])) {
+            throw new Exception("Bad Request, User email required");
+        }
+    }
+}
+
+$data = $_GET;
+$user = new User($data);
+try {
+    $user->validate($data);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+print_r($user->formatJson());
