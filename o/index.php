7665bebc00493ab7c631df76dfd29269cf6f41bb
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-01 22:27:22 +0500

    Wrong code: breaking Open Closed principle

--- o/index.php
+++ o/index.php
@@ -1,3 +1,39 @@
 <?php
 
+class Square
+{
+    public int|float $height;
+    public int|float $width;
+}
+
+class Circle
+{
+    public int|float $radius;
+}
+
+class Triangle
+{
+    public int|float $base;
+    public int|float $height;
+}
+
+class AreaCalculator
+{
+    public function calculate($shapes)
+    {
+        $area = [];
+
+        foreach ($shapes as $shape) {
+            if ($shape instanceof Square)
+                $area[] = $shape->height * $shape->width;
+            elseif ($shape instanceof Circle)
+                $area[] = pi() + ($shape->radius * $shape->radius);
+            elseif ($shape instanceof Triangle)
+                $area[] = ($shape->height * $shape->base) / 2;
+        }
+
+        return array_sum($area);
+    }
+}
+
 echo "Prepared for Open Closed principle";
