076ad506fe68174848446db4201c672f049c38cd
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-01 23:18:28 +0500

    feat: updated file hierarchy

--- s/index.php
+++ s/index.php
@@ -1,7 +1,70 @@
 <?php
-require_once('User.php');
-require_once('UserRequest.php');
-require_once('Json.php');
+class Json
+{
+    /**
+     * Encode to json
+     *
+     * @param $data
+     * @return false|string
+     */
+    public static function from($data): false|string
+    {
+        return json_encode($data);
+    }
+}
+
+class User
+{
+    /**
+     * @var string
+     */
+    public string $name;
+    /**
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * User constructor
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+    }
+}
+
+class UserRequest
+{
+    /**
+     * Rules list
+     *
+     * @var array
+     */
+    protected static $rules = [
+        'name' => 'string',
+        'email' => 'string',
+    ];
+
+    /**
+     * Validate user data
+     *
+     * @param $data
+     * @return bool
+     * @throws Exception
+     */
+    public static function validate($data): bool
+    {
+        foreach (self::$rules as $property => $type) {
+            if (gettype($data[$property]) != $type) {
+                throw new Exception("Bad Request, User property {$property} must be of type {$type}");
+            }
+        }
+        return true;
+    }
+}
 
 $data = $_GET;
 $user = new User($data);
